0141906a32176d26cd0a8a5fd04b24b8255dc169
DEIDRE <deidrepizarro@gmail.com>
2025-11-15 22:37:59 +0100

    agregamos proyecto

---
+++ IMC.py
@@ -0,0 +1,19 @@
+
+
+#practicas de tipo de datos y entradas de usuario
+#crear un programa que calcule el indice de masa corporal de una persona
+
+
+
+print("vamos al calcular tu masa corporal")
+
+peso= input("cual es tu peso en kg?: ")
+estatura= input("cual es tu estatura en metros? ")
+imc= float(peso) / (float(estatura) ** 2)                
+print("tu indice de masa corporal es: " + str(imc))
+
+
+
+
+
+
